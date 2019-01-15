c8f5d2cee74811ec5edf7740bd8d3d33a903f4d0
Maxi Lagos <maximiliano.lagos@databricks.com>
2019-01-15 15:47:30 -0300

    Adding path traversal vulnerability

--- src/main/ScalaExample.scala
+++ src/main/ScalaExample.scala
@@ -1,3 +1,6 @@
+import java.nio.file.{Files, Paths}
+
+import scala.io.Source
 import scala.util.Random
 
 class ScalaExample {
@@ -8,5 +11,22 @@ class ScalaExample {
   def generateSecretToken() {
     val result = Seq.fill(16)(Random.nextInt)
     return result.map("%02x" format _).mkString
+  }
+
+  def getWordList(value:String) {
+    if (!Files.exists(Paths.get("public/lists/" + value))) {
+      NotFound("File not found")
+    } else {
+      val result = Source.fromFile("public/lists/" + value).getLines().mkString // Weak point
+      Ok(result)
+    }
+  }
+
+  def NotFound(str: String): Unit = {
+    print(str);
+  }
+
+  def Ok(str: String): Unit = {
+    print(str);
   }
 }
