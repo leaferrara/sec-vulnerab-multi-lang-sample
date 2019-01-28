cdc2ee97fa4a62e79d7ef19d746a940a661b7bcb
Leandro Ferrara <leandro.ferrara@databricks.com>
2019-01-28 11:47:52 -0300

    Duplicated to see if catched on PR.

---
+++ src/main/ScalaExample2.scala
@@ -0,0 +1,43 @@
+import java.nio.file.{Files, Paths}
+import java.sql.SQLData
+
+import sun.nio.cs.Surrogate.Parser
+
+import scala.io.Source
+import scala.util.Random
+import scala.sys.process._
+import java.sql.DriverManager
+
+import javax.swing.text.html.HTML
+
+class ScalaExample[value3:HTML] {
+
+  val stringExample = "example";
+
+
+  // Path traversal
+  def getWordList2(value:String) {
+    if (!Files.exists(Paths.get("public/lists/" + value))) {
+      NotFound("File not found")
+    } else {
+      val result = Source.fromFile("public/lists/" + value).getLines().mkString // Weak point
+      Ok(result)
+    }
+  }
+
+  //  def doGet(value:String) {
+  //    Ok("Hello " + value + " !").as("text/html")
+  //  }
+
+  def NotFound(str: String): Unit = {
+    print(str);
+  }
+
+  def Ok(str: String) = {
+    print(str);
+
+    def as(arg:String): Unit = {
+      print();
+    }
+  }
+}
