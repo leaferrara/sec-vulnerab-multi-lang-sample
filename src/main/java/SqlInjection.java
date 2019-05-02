2180204d4314045fd9d2d3c6dd60ee05ea53f59d
Leandro Ferrara <leandro.ferrara@databricks.com>
2019-05-02 12:12:12 -0300

    Code tidying up.

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -1,32 +1,43 @@
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class SqlInjection {
 
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType('text/html;charset=UTF-8');
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
 
-            String user = request.getParameter('user');
+            String user = request.getParameter("user");
             Connection conn = null;
-            String url = 'jdbc:mysql://192.168.2.128:3306/';
-            String dbName = 'anvayaV2';
-            String driver = 'com.mysql.jdbc.Driver';
-            String userName = 'root';
-            String password = '';
+            String url = "jdbc:mysql://192.168.2.128:3306/";
+            String dbName = "anvayaV2";
+            String driver = "com.mysql.jdbc.Driver";
+            String userName = "root";
+            String password = "";
             try {
                 Class.forName(driver).newInstance();
                 conn = DriverManager.getConnection(url + dbName, userName, password);
 
                 Statement st = conn.createStatement();
                 String query = "SELECT * FROM  User where userId='" + user + "'";
-                out.println('Query : ' + query);
+                out.println("Query : " + query);
                 System.out.printf(query);
                 ResultSet res = st.executeQuery(query);
 
-                out.println('Results');
+                out.println("Results");
                 while (res.next()) {
-                    String s = res.getString('username');
-                    out.println('\t\t' + s);
+                    String s = res.getString("username");
+                    out.println("\t\t" + s);
                 }
                 conn.close();
 
